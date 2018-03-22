3bda937358ffa993a010ec302ff10e89c4baf0e7
SashaKor <alexandra.korukova@gmail.com>
2018-03-22 11:04:43 +0100

    The rest of the constructors and == operator implemented

--- cstring.cpp
+++ cstring.cpp
@@ -2,7 +2,9 @@
 // Created by Shon on 14/03/18.
 //
 #define EOS '\0'
+#define BUFFER_SIZE 1024
 #include "cstring.h"
+#include "stdexcept"
 
 String::String() {
     length = 0;
@@ -10,7 +12,7 @@ String::String() {
     string[0] = EOS;
 }
 
-String::String(char* str){
+String::String(const char* str){
     length = 0;
     while(str[length] != EOS) {
         length++;
@@ -18,10 +20,6 @@ String::String(char* str){
     string = new char[length+1];
 
     memcpy(string, str, length+1);
-
-    for (size_t i = 0; i <= length; ++i) {
-        string[i] = str[i];
-    }
 }
 
 String::String(const String& str) {
@@ -42,21 +40,57 @@ String& String::operator=(const String &str) {
     return *this;
 }
 
-String::String(const char c) {
+String& String::operator=(const char *str){
+    String toAssign(str);
+    return (*this) = toAssign;
+}
 
+String::String(const char c) {
+    length = 1;
+    string = new char[length+1];
+    string[0] = c;
+    string[1] = EOS;
 }
 
 String::String(const int i) {
+    char buffer[BUFFER_SIZE];
+    int l;
+    length = (size_t)sprintf(buffer, "%d", i);
     string = new char[length+1];
-    sprintf()
+    memcpy(string, buffer, length+1);
 }
 
 String::String(const double d) {
-
+    char buffer[BUFFER_SIZE];
+    length = (size_t)sprintf(buffer, "%f", d);
+    string = new char[length+1];
+    memcpy(string, buffer, length+1);
 }
 
 String::String(const bool b) {
+    if(b) {
+        *this = String("true");
+    } else {
+        *this = String("false");
+    }
+}
 
+bool String::operator==(const String &str) const {
+    if(length == str.length && !memcmp(string, str.string, length)) {
+        return true;
+    }
+    return false;
+}
+
+bool String::equals(const String &str) const {
+    return *this == str;
+}
+
+char& String::at(size_t i) {
+    if(i >= length) {
+        throw std::out_of_range("String::at() : index out of range");
+    }
+    return string[i];
 }
 
 std::ostream& operator<<(std::ostream& os, const String& str) {
